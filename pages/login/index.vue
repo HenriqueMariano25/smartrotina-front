78a6df038d539401878082119702e0a09dc6c236
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2025-03-22 14:51:50 -0300

    Inicia o projeto

---
+++ pages/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex">
+    <div class="w-1/2 bg-fundo">
+      
+    </div>
+    <div class="h-screen w-1/2 overflow-hidden ">
+      <NuxtImg src="images/tela-inicial.jpg" class="w-full h-full object-cover object-center"/>
+      <div class="absolute justify-self-end top-0 inset-0 bg-gradient-to-r from-[#E8E9E3] to-transparent w-1/2 "/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "red",
+        fundo: "#E8E9E3"
+      }
+    }
+  }
+}
