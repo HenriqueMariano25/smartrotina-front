147b338b11a2c3244cf4d1f094cafaa6b0598e16
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2025-03-22 15:56:35 -0300

    Cria base das paginas de login e cadastrar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura'
+import {definePreset} from "@primeuix/styled";
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#e8f2f2',
+      100: '#b9d8d9',
+      200: '#8bbebf',
+      300: '#5ca4a5',
+      400: '#2d8a8c',
+      500: '#167d7f',
+      600: '#147172',
+      700: '#126466',
+      800: '#0f5859',
+      900: '#0b3f40',
+      950: '#093233'
+    }
+  }
+});
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module'
+  ],
+  css: ["~/assets/css/primevue-theme.css"],
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: MyPreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: false,
+          cssLayer: false
+        }
+      }
+    }
+  },
 })
--- pages/cadastrar/index.vue
+++ pages/cadastrar/index.vue
@@ -3,8 +3,12 @@
     <div class="w-1/2 bg-fundo items-center flex justify-center">
       <div class="flex flex-col gap-4 w-6/12">
         <img src="@/assets/images/logo.png" alt="Logo" class="mb-4"/>
-        <span class="text-4xl font-bold text-center ">Entrar na conta</span>
-        <span class="text-center">Entre e continue tornando seu lar mais prático e organizado.</span>
+        <span class="text-4xl font-bold text-center ">Criar conta</span>
+        <span class="text-center">Crie sua conta e descubra uma nova forma de gerenciar seu lar!</span>
+        <FloatLabel variant="on">
+          <InputText name="nome" class="w-full"/>
+          <label for="nome">Nome</label>
+        </FloatLabel>
         <FloatLabel variant="on">
           <InputText name="email" class="w-full"/>
           <label for="email">Email</label>
@@ -13,15 +17,14 @@
           <InputText type="password" name="senha" class="w-full"/>
           <label for="senha">Senha</label>
         </FloatLabel>
-        <Button label="Entrar" severity="primary-50"/>
-        <NuxtLink to="/cadastrar" class="text-center">
-          <span class="text-center text-sm">Não tem conta ainda ? <b>Criar</b></span>
+        <Button label="Criar" severity="primary-50" />
+        <NuxtLink to="/login" class="text-center">
+          <span class="text-sm">Já tem conta ? <b>Entrar</b></span>
         </NuxtLink>
       </div>
     </div>
     <div class="h-screen w-1/2 overflow-hidden ">
-      <img src="@/assets/images/tela-inicial.jpg" class="w-full h-full object-cover object-center"
-           alt="imagem lateral com uma lista de compras"/>
+      <img src="@/assets/images/tela-inicial.jpg" class="w-full h-full object-cover object-center" alt="imagem lateral com uma lista de compras"/>
       <div class="absolute justify-self-end top-0 inset-0 bg-gradient-to-r from-[#E8E9E3] to-transparent w-1/2 "/>
     </div>
   </div>
